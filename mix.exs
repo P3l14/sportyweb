0bbde510eaf638b85225ee6b1d788e2cca5c856a
Patric Weitzel <patric.l.w@gmail.com>
2026-05-12 15:19:17 +0200

    mix
    Added alias for pre commit checks

--- mix.exs
+++ mix.exs
@@ -155,6 +155,7 @@ defmodule Sportyweb.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       "directory.setup": ["directory.banks.import", "directory.streets.import"],
+      "pre.commit.checks": ["compile", "format", "credo --all"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
       "assets.build": ["tailwind sportyweb", "esbuild sportyweb"],
