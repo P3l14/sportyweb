8252ab6e3a03257d4de3485613b85827b680ccac
Patric Weitzel <patric.l.w@gmail.com>
2026-06-18 09:32:57 +0200

    contact_roles
    Space formatting on new role config entries.

--- lib/sportyweb/personal/contact_role.ex
+++ lib/sportyweb/personal/contact_role.ex
@@ -75,8 +75,8 @@ defmodule Sportyweb.Personal.ContactRole do
         has_role_relation: :group
       ],
       [key: "Vereinsmitarbeiter", value: "club staff", custom_input: true],
-      [key: "Trainer", value: "coach",  has_role_relation: :department],
-      [key: "Übungsleiter", value: "sport instructor",  has_role_relation: :department],
+      [key: "Trainer", value: "coach", has_role_relation: :department],
+      [key: "Übungsleiter", value: "sport instructor", has_role_relation: :department],
       [key: "Kassenprüfer", value: "cash auditor", requires_membership: true],
       [key: "Schriftführer", value: "secretary", requires_membership: true],
       [key: "Jugendleiter", value: "youth leader", requires_membership: true],
